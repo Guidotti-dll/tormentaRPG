dd7958eeea10036da9064c05ec516f2fb4ed45c6
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-01-15 17:59:36 -0300

    feat: config eslint frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  distDir: 'build',
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    apiUrl: 'http://signup-api:3000'
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    apiUrl: 'http://localhost:3000'
-  }
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
